01456370c2ef4add808a6db57e5f1c5395abd220
OUSMANE Mohamed Sadjad <ousmanesadjad@gmail.com>
2026-03-31 04:32:34 +0200

    feat(adapters): implement OperationDispatcher with built-in target validation (BBOARD-64)

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -4,4 +4,5 @@ export * from './base-adapter.js'
 export * from './input-adapter.js'
 export * from './textarea-adapter.js'
 export * from './contenteditable-adapter.js'
+export * from './dispatcher.js'
 
