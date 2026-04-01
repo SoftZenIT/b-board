8d4ab43afdde9260fe527b267abe75f41473824f
OUSMANE Mohamed Sadjad <ousmanesadjad@gmail.com>
2026-04-01 17:20:58 +0200

    feat(arch): implement runtime module structure exports (BBOARD-73)

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -1,8 +1,5 @@
-export * from './types.js'
-export * from './target-validator.js'
-export * from './base-adapter.js'
-export * from './input-adapter.js'
-export * from './textarea-adapter.js'
-export * from './contenteditable-adapter.js'
-export * from './dispatcher.js'
-
+export * from './types.js';
+export * from './base-adapter.js';
+export * from './input-adapter.js';
+export * from './textarea-adapter.js';
+export * from './contenteditable-adapter.js';
